cb78a69c88c628b191e3a2f775040e25dac310aa
Rahulhawlader <hawladerrahul8@gmail.com>
2022-11-04 00:59:09 +0600

    booking section

--- src/Pages/Home/Booking/Booking.js
+++ src/Pages/Home/Booking/Booking.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import './Booking.css'
+import table from '../../../image/menu items/table.png'
+import kitChent from '../../../image/menu items/our kitchen.png'
+import menu from '../../../image/menu items/our menu.png'
 
 const Booking = () => {
  return (
-  <div className='container mt-5'>
+  <div className='container booking-main-part'>
    <h1 className='text-center text-white'> Booking</h1>
-   
+   <div className='row styless justify-content-center'>
+    <div className='card-box col-12 col-md-6 col-lg-4'>
+     <div className='img-dev'>
+       <img src={table} alt='table'></img>
+     </div>
+       <button>Booking Table</button>
+    </div>
+    <div className='card-box col-12 col-md-6 col-lg-4'>
+     <div className='img-dev'>
+       <img src={menu } alt='table'></img>
+     </div>
+       <button>Our Menu</button>
+    </div>
+    <div className='card-box col-12 col-md-6 col-lg-4'>
+     <div className='img-dev'>
+       <img src={kitChent} alt='table'></img>
+     </div>
+       <button>Our Kitchen</button>
+    </div>
+
+   </div>
    
   </div>
  );
